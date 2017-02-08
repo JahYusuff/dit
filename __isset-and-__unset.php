4495173d9da37f06de2459551102e0bc7d5f4104
Sam Smith <yusuff007@gmail.com>
2017-02-08 17:12:19 +0530

    sdfsdf

--- __isset-and-__unset.php
+++ __isset-and-__unset.php
@@ -1,17 +1,7 @@
 <?php
-class test
-{
-  function __isset($name)
-  {
-      echo "__isset is called for $name";
-  }
-  function __unset($name)
-  {
-      echo "__unset is called for $name";
-  }
-}
-$a = new test();
-isset($a->x);
-unset($a->c);
+yusuff
+hi am jabagst yisisdfj
+sjkdafhl
+
 
 ?>
