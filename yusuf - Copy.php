458ad971e715594f92f16774713189a31b632420
Sam Smith <yusuff007@gmail.com>
2017-02-08 14:37:22 +0530

    Message Sam Smith

--- yusuf - Copy.php
+++ yusuf - Copy.php
@@ -1,17 +1,4 @@
 <?php
-class test
-{
-  function __isset($name)
-  {
-      echo "__isset is called for $name";
-  }
-  function __unset($name)
-  {
-      echo "__unset is called for $name";
-  }
-}
-$a = new test();
-isset($a->x);
-unset($a->c);
+hi, am jahabar yusuff chennai, working as a php developer for the past 20 yerar insdklfnaskf 
 
 ?>
